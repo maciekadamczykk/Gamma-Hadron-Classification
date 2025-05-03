432e282fa5319d33e606b345565b231f2fc6c3ba
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-03 17:21:18 +0200

    Plotting features in histogram

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import matplotlib.pyplot as plt
 cols = ["fLength", "fwidth", "fSize", "fConc","fConc1", "fAsym", "fM3Long","fM3Trans","fAlpha", "fDist", "class"]
 df = pd.read_csv("magic04.data", names=cols)
 
-#g stands for 1 and h stands for 0
+#1 stands for g and 0 stands for h
 df["class"] = (df["class"] == "g").astype(int)
-print(df.head())
+
+for label in cols[:-1]:
+    plt.hist(df[df["class"] == 1][label], color="blue", label="gamma", alpha=0.7, density=True)
+    plt.hist(df[df["class"] == 0][label], color="red", label="tetha", alpha=0.7, density=True)
+    plt.title(label)
+    plt.ylabel("Probability")
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
+
+
+
