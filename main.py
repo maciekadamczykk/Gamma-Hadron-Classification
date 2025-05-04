f126ccaac11c96cf359348bf59902cf7684851af
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-04 11:33:41 +0200

    Splitting into train, valid and test set

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ df["class"] = (df["class"] == "g").astype(int)
 
 for label in cols[:-1]:
     plt.hist(df[df["class"] == 1][label], color="blue", label="gamma", alpha=0.7, density=True)
-    plt.hist(df[df["class"] == 0][label], color="red", label="tetha", alpha=0.7, density=True)
+    plt.hist(df[df["class"] == 0][label], color="red", label="hadron", alpha=0.7, density=True)
     plt.title(label)
     plt.ylabel("Probability")
     plt.xlabel(label)
     plt.legend()
-    plt.show()
+    #plt.show()
 
+
+train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
 
 
